a8a5db54c45bf8cf883be66f8b3e8d0beb876b85
Nozdap <herveuxr@gmail.com>
2018-08-23 23:21:33 +0200

    Add more answers

--- imports/answers.js
+++ imports/answers.js
@@ -1,7 +1,14 @@
 const Answers = [
-    "Tu le tiens entre tes mains, andouille !",
-    "Sous tes yeux ?",
-    "Pose ce que tu es en train de tenir, fais un tour sur toi-même, et ton téléphone apparaîtra sous tes yeux."
+    'Tu le tiens entre tes mains, andouille !',
+    'Sous tes yeux.',
+    'Pose ce que tu es en train de tenir, fais un tour sur toi-même. Ton téléphone est maintenant sous tes yeux.',
+    'Regarde à gauche, puis à droite. Maintenant regarde moi ? Tadaaaa!!',
+    'Là.',
+    'Il ne devrait pas être loin 🤔',
+    'J\'ai bien une réponse pour toi, mais elle ne va pas te plaire...',
+    'Je ne sais pas, désolé :/',
+    'Attention derrière toi'.toLocaleUpperCase(),
+    'Abracadabra, ton cellulaire est dans tes bras !',
 ]
 
 export default Answers;
